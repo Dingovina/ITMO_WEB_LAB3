31b34dda5bc4563e995118a6b2cb1388f0d0c903
Dingovina <kortkostya222@gmail.com>
2024-12-07 09:20:55 +0300

    Add dbManager interface

--- app/src/main/java/app/Model/DataBaseManager.java
+++ app/src/main/java/app/Model/DataBaseManager.java
@@ -1,99 +1,12 @@
 package app.Model;
 
-import java.sql.Statement;
-import java.time.ZonedDateTime;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import app.dto.PointDTO;
-import jakarta.annotation.ManagedBean;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Named;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+public interface DataBaseManager {
+    boolean addPoint(PointDTO point) throws SQLException;
 
-@Named("dbManager")
-@ManagedBean
-@ApplicationScoped
-public class DataBaseManager {
-    private String HOST_NAME = "localhost";
-    private String PORT = "5432";
-    private String DB_NAME = "lab3storage";
-    private String TABLE_NAME = "points";
-
-    
-    private String url = "jdbc:postgresql://" + HOST_NAME + ":" + PORT + "/" + DB_NAME;
-    private Properties props = new Properties();
-    
-    private static Connection connection;
-    private Statement statement;
-    
-    public DataBaseManager() throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
-        Class.forName("org.postgresql.Driver");
-        props.load(new FileInputStream("db.cfg"));
-        if (connection == null){
-            connection = DriverManager.getConnection(url, props);
-        }
-        statement = connection.createStatement();
-        createTable();        
-    }
-
-    private void createTable() throws SQLException {
-        System.out.println("Creating table...");
-        String query = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (\n" +
-                        "  id SERIAL PRIMARY KEY,\n" +
-                        "  x double precision,\n" +
-                        "  y double precision,\n" +
-                        "  r double precision,\n" +
-                        "  hit boolean,\n" +
-                        "  drawn boolean,\n" +
-                        "  time varchar(255)\n" +
-                        ")";
-        statement.executeUpdate(query);
-    }
-    
-    public boolean addPoint(PointDTO point) throws SQLException {
-        createTable();
-        String query = "INSERT INTO " + TABLE_NAME + " (x, y, r, hit, drawn, time)\n" +
-                        "VALUES (?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(query)) {
-            ps.setDouble(1, point.getX());
-            ps.setDouble(2, point.getY());
-            ps.setDouble(3, point.getR());
-            ps.setBoolean(4, point.isHit());
-            ps.setBoolean(5, point.isDrawn());
-            ps.setString(6, point.getTime().toString());
-            return ps.executeUpdate() == 1;
-        }
-        catch (SQLException e) {
-            return false;
-        }
-    }
-    
-    public ArrayList<PointDTO> getPoints() throws SQLException {
-        createTable();
-        String query = "SELECT x, y, r, hit, time\n" +
-                        "FROM " + TABLE_NAME;
-        ArrayList<PointDTO> list = new ArrayList<>();
-        PreparedStatement ps = connection.prepareStatement(query);
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            PointDTO point = new PointDTO();
-            point.setX(rs.getDouble(1));
-            point.setY(rs.getDouble(2));
-            point.setR(rs.getDouble(3));
-            point.setHit(rs.getBoolean(4));
-            point.setTime(ZonedDateTime.parse(rs.getString(5)));
-            list.add(point);
-        }
-        return list;
-    }
+    ArrayList<PointDTO> getPoints() throws SQLException;
 }
-
